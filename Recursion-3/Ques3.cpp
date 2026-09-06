2e63ecbca934a081c89403d1deb5a6672314526a
Shubh Pratap Singh <shubhpratapsingh12@gmail.com>
2026-09-06 17:14:14 +0530

    Add files via upload

---
+++ Recursion-3/Ques3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+// void printMax(int arr[], int n, int idx, int max){
+//     if(idx==n){  // Base Case
+//         cout<<max;
+//         return;
+//     }
+//     if(max<arr[idx]) max = arr[idx];
+//     printMax(arr,n,idx+1,max);
+// }
+
+int maxInArray(int arr[], int n, int idx){
+    if(idx==n) return INT_MIN;
+    return max(arr[idx], maxInArray(arr,n,idx+1));
+}
+int main(){
+    int arr[] = {2,1,6,3,9,0,2,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    // printMax(arr,n,0,INT_MIN);
+    cout<<maxInArray(arr,n,0);
+}
